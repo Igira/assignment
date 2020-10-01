82e7253700ba23e5ef47575d07fa9e9b8ed1c5c0
igira <eddygucci256@gmail.com>
2020-10-01 17:07:22 +0300

    Updates

---
+++ precache-manifest.c7f5380c64fd0d8d515e0658aea9e3c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16eafa5269c1a7f3284808d40438934",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "65b0e0b64dc665edee3f",
+    "url": "/assignment/static/css/main.d70fac12.chunk.css"
+  },
+  {
+    "revision": "79e4405c65d6f275da4d",
+    "url": "/assignment/static/js/2.66c8c5ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assignment/static/js/2.66c8c5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b0e0b64dc665edee3f",
+    "url": "/assignment/static/js/main.2c6392f3.chunk.js"
+  },
+  {
+    "revision": "5d742f5966ada52d9949",
+    "url": "/assignment/static/js/runtime-main.205c1389.js"
+  }
+]);
